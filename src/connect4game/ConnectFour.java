ac0ed59068e7360e8da2da600b3dcf13615df2f0
Mikolaj Zawisza <mikolaj.zawisza.96@gmail.com>
2019-01-19 21:55:09 +0100

    Initial Commit

---
+++ src/connect4game/ConnectFour.java
@@ -0,0 +1,8 @@
+package connect4game;
+
+public class ConnectFour {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/connect4game/ConnectFourControl.java
@@ -0,0 +1,4 @@
+package connect4game;
+
+public class ConnectFourControl {
+}
